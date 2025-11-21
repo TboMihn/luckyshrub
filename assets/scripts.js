121ec50f4f18e168bb5b838a568f96b62c21fe80
TboMihn <tlfinn1618@gmail.com>
2025-11-21 14:48:54 -0700

    Added my_submit function to the JS file that handles the button click on the contact form and gives a thank you message.

--- assets/scripts.js
+++ assets/scripts.js
@@ -3,9 +3,17 @@
 // submit function to hide the form and show a thank you message
 
 function my_submit() {
-	const name = document.getElementById("customer_first_name");
+	event.preventDefault();
+	const firstName = document.getElementById("customer_name");
 	const lastName = document.getElementById("customer_last_name");
-	const fullname = name.value + " " + lastName.value;
+	const contact_form = document.getElementById("lucky_contact_form");
+	const thank_you_message = document.getElementById("contact_response");
 
-	console.log(fullname);
+	contact_form.style.display = "none";
+	thank_you_message.style.display = "block";
+	thank_you_message.innerHTML = `<h2>Thank you, ${firstName.value}, for contacting us.</h2>
+        <p>We will respond to your email shortly.</p>
+        <p>Have a great day!</p>`;
+	// const fullname = `${firstName.value} ${lastName.value}`;
+	// console.log(fullname);
 }
