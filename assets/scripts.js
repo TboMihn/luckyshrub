b8bafb3d1d680eb4d543b8b0b16653a752d3c3f4
TboMihn <tlfinn1618@gmail.com>
2025-11-21 14:03:32 -0700

    fixed the script pointer in the head of all files.

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,1 +1,11 @@
+// form function that checks each field.value > 2 and if valid enables the submit button
 
+// submit function to hide the form and show a thank you message
+
+function my_submit() {
+	const name = document.getElementById("customer_first_name");
+	const lastName = document.getElementById("customer_last_name");
+	const fullname = name.value + " " + lastName.value;
+
+	console.log(fullname);
+}
